9f76c937a17ab391699be8dc5d30ee0379a8be67
hoho715 <andy2001@g.hongik.ac.kr>
2025-09-23 11:42:42 +0900

    fix: 회원가입 페이지 라우팅 오류 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import MainPage from './pages/MainPage.jsx';
 import Login from './pages/Login.jsx';
-import IntroIeum from './pages/IntroIeum.jsx'; // IntroIeum 컴포넌트 import
+import IntroIeum from './pages/IntroIeum.jsx';
+import SignUp from './pages/SignUp.jsx'; // SignUp 컴포넌트 import
 
 function App() {
   return (
@@ -17,12 +18,14 @@ function App() {
         </>
       } />
       <Route path="/login" element={<Login />} />
-      <Route path="/about/ieum" element={ // 라우트 추가
+      <Route path="/about/ieum" element={
         <>
           <Header />
           <IntroIeum />
         </>
       } />
+      {/* SignUp 페이지 라우트 추가 */}
+      <Route path="/signup" element={<SignUp />} /> 
     </Routes>
   );
 }

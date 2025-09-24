153fec2ba71733bed7c7da14bae3539487ea044f
hoho715 <andy2001@g.hongik.ac.kr>
2025-09-24 19:22:35 +0900

    fix: 회원가입 페이지로 이동하지 않는 오류 수정
    - App.jsx 파일에 SignUp 페이지에 대한 라우트(/signup)를 추가했습니다.
    - Login.jsx의 '회원가입' 링크가 올바른 경로로 연결되도록 Link 컴포넌트의 to 속성을 수정했습니다.
    - 이제 로그인 페이지에서 회원가입 버튼을 클릭하면 SignUp 페이지로 정상적으로 이동합니다.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from './pages/Login.jsx';
 import IntroIeum from './pages/IntroIeum.jsx';
 import InputStart from './pages/Input.jsx'; // 기존 Input 컴포넌트 이름 변경
 import InputMeasure from './pages/InputMeasure.jsx'; // InputMeasure 컴포넌트 import
+import SignUp from './pages/SignUp.jsx'; // SignUp 컴포넌트 import
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
       } />
       <Route path="/input" element={<InputStart />} /> // 시작 페이지 라우트
       <Route path="/input/measure" element={<InputMeasure />} /> // 입력 페이지 라우트
+      <Route path="/signup" element={<SignUp />} /> // SignUp 라우트 추가
     </Routes>
   );
 }

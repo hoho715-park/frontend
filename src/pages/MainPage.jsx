4b9af675f95929dd1cfc062c3961804a18605bd5
hoho715 <andy2001@g.hongik.ac.kr>
2025-09-24 11:06:27 +0900

    refactor: 메인 페이지 버튼 애니메이션 및 UI 개선
    - '테스트하기' 버튼에 마우스 오버 시 나타나는 이미지의 위치를 각 버튼의 좌측/우측 상단으로 정확히 조정했습니다.
    - 마우스 오버 시 이미지가 좌우로 흔들리는 애니메이션을 복구하고, 위치에 따라 애니메이션이 올바르게 동작하도록 수정했습니다.
    - `transform` 속성으로 인한 위치 오류를 해결하고, `position` 속성을 활용해 정확한 위치를 지정했습니다.
    - 메인 페이지의 시각적 요소와 사용자 경험을 최종적으로 개선했습니다.

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,6 +4,15 @@ import './MainPage.css';
 
 const MainPage = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleMouseEnterButton = (buttonName) => {
+    setActiveButton(buttonName);
+  };
+
+  const handleMouseLeaveButton = () => {
+    setActiveButton(null);
+  };
 
   return (
     <>
@@ -11,7 +20,7 @@ const MainPage = () => {
         <div className="main-text-container">
           <h1>“이음, 나의 체질과 하루를 잇다”</h1>
           <p>당신의 체질을 분석해 건강한 하루를 설계합니다.</p>
-          <div className="expand-title">나의 사상체질 알아보기!</div> {/* 텍스트를 버튼 위로 이동 */}
+          <div className="expand-title">나의 사상체질 알아보기!</div>
           <div className="test-button-wrapper"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}>
@@ -20,15 +29,33 @@ const MainPage = () => {
             </div>
             <div className={`test-button-expand ${isHovered ? 'visible' : ''}`}>
               <div className="expand-buttons">
-                <Link to="/test/qsc-survey" className="expand-button-1">
-                  QSCC 설문으로 알아보기
+                <Link
+                  to="/test/qsc-survey"
+                  className="expand-button-1"
+                  onMouseEnter={() => handleMouseEnterButton('survey')}
+                  onMouseLeave={handleMouseLeaveButton}>
+                  <div className="button-text-wrapper">
+                    QSCC 설문으로 알아보기
+                  </div>
                 </Link>
-                <Link to="/input" className="expand-button-2">
-                  손바닥 장기 수치로 알아보기
+                <Link
+                  to="/input"
+                  className="expand-button-2"
+                  onMouseEnter={() => handleMouseEnterButton('hand')}
+                  onMouseLeave={handleMouseLeaveButton}>
+                  <div className="button-text-wrapper">
+                    손바닥 장기 수치로 알아보기
+                  </div>
                 </Link>
               </div>
             </div>
           </div>
+          {activeButton === 'survey' && (
+            <img src="/mainpage_survey.png" alt="설문지 아이콘" className="button-icon-survey animating" />
+          )}
+          {activeButton === 'hand' && (
+            <img src="/mainpage_hand.png" alt="손 아이콘" className="button-icon-hand animating" />
+          )}
         </div>
       </section>
     </>
